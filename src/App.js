43016aaf795ea5767d8d9207044bdfe2cd36d81d
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-10 18:50:07 +0530

    refactor: moved css import links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import "./App.css";
+import "./styles/layouts/scrollbar.css";
+import "react-toastify/dist/ReactToastify.min.css";
+
 import { PageRoutes } from "./routes/PageRoutes";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={2000} />
       <PageRoutes />
     </div>
   );
