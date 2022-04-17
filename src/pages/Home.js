7a3b3f602b20ce52c4db38394550bc240d8598eb
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-04-17 22:45:38 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,14 @@ import { NotesCard } from "../components/NotesCard";
 import { EditNotesCard } from "../components/EditNotesCard";
 import { useNotes } from "../context/notes-context";
 import { Sidebar } from "../components/Sidebar";
+import { FiltersBox } from "../components/FiltersBox";
+import { getFilteredNotes } from "../utilities/getFilteredNotes";
+import { useFilter } from "../context/filter-context";
+import { toast } from "react-toastify";
 
 export const Home = () => {
   const { notesState } = useNotes();
+  const { filterState, dispatchFilter } = useFilter();
 
   const pinnedNotesList = notesState.notesList.filter(
     (note) => note.isPinned && !note.isArchived && !note.isTrashed
@@ -15,30 +20,52 @@ export const Home = () => {
   const othersNotesList = notesState.notesList.filter(
     (note) => !note.isPinned && !note.isArchived && !note.isTrashed
   );
+
+  const allNotesList = notesState.notesList.filter((note) => !note.isTrashed);
+  const filteredNotesList = getFilteredNotes(filterState, allNotesList);
+
   return (
     <>
       <Navbar />
       <EditNotesCard />
+      <FiltersBox />
 
       <div className="home-page-container flex">
         <Sidebar />
 
         <div className="notes-listing flex-column-center">
-          <div className="fw-bold mg-y-sm">
-            PINNED ({pinnedNotesList.length})
-          </div>
+          {filterState.isFiltering && (
+            <div className="fw-bold mg-y-sm">
+              Filtered Notes ({filteredNotesList.length})
+              <a
+                className="filter-clear-btn mg-x-xsm"
+                onClick={() => {
+                  dispatchFilter({ type: "CLEAR-FILTERS" });
+                  toast.success("Filter Cleared");
+                }}
+              >
+                CLEAR
+              </a>
+              {filteredNotesList.map((note) => (
+                <NotesCard key={note._id} noteDetails={note} />
+              ))}
+            </div>
+          )}
 
-          {pinnedNotesList.map((note) => (
-            <NotesCard key={note._id} noteDetails={note} />
-          ))}
-
-          <div className="fw-bold mg-y-sm">
-            OTHERS ({othersNotesList.length})
-          </div>
-
-          {othersNotesList.map((note) => (
-            <NotesCard key={note._id} noteDetails={note} />
-          ))}
+          {!filterState.isFiltering && (
+            <div className="fw-bold mg-y-sm">
+              PINNED ({pinnedNotesList.length})
+              {pinnedNotesList.map((note) => (
+                <NotesCard key={note._id} noteDetails={note} />
+              ))}
+              <div className="fw-bold mg-y-sm">
+                UNPINNED ({othersNotesList.length})
+              </div>
+              {othersNotesList.map((note) => (
+                <NotesCard key={note._id} noteDetails={note} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </>
