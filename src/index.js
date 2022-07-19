55b23162bd54282ec48d7f49fcda139b8459bf82
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-19 15:02:46 +0530

    feat: added auth provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { NotesProvider } from "./context/notes-context";
 import App from "./App";
 import { makeServer } from "./server";
 import { FilterProvider } from "./context/filter-context";
+import { AuthProvider } from "./context/auth-context";
 
 // Call make Server
 makeServer();
@@ -12,11 +13,13 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <NotesProvider>
-        <FilterProvider>
-          <App />
-        </FilterProvider>
-      </NotesProvider>
+      <AuthProvider>
+        <NotesProvider>
+          <FilterProvider>
+            <App />
+          </FilterProvider>
+        </NotesProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
