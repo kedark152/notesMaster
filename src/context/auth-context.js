60b292d2640307f28e295f921c97be55766f24a8
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-19 14:57:33 +0530

    created auth-context

---
+++ src/context/auth-context.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { useState, useContext, createContext, useEffect } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    token
+      ? setAuth({ token: token, isLoggedIn: true })
+      : setAuth({ token: "", isLoggedIn: false });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+export { AuthProvider, useAuth };
