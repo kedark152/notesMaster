3b3e4eee75585edd7368b827210aa98269703a17
Kedar Kulkarni <82270276+kedark152@users.noreply.github.com>
2022-07-19 14:53:44 +0530

    Update ColorBox.js

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -3,9 +3,12 @@ import "../styles/utils/variable.css";
 import "../styles/component/colorbox.css";
 import { useNotes } from "../context/notes-context";
 import { v4 as uuid } from "uuid";
+import { updateNoteBgColor } from "../services/notesServices";
+import { useAuth } from "../context/auth-context";
 
-export const ColorBox = ({ _id }) => {
-  const { dispatchNotes } = useNotes();
+export const ColorBox = ({ _id, setColorBox }) => {
+  const { notesState, dispatchNotes } = useNotes();
+  const { auth } = useAuth();
   const noteColorsList = [
     "noteWhite",
     "noteRed",
@@ -17,20 +20,39 @@ export const ColorBox = ({ _id }) => {
     "noteGrad2",
     "noteGrad3",
   ];
+  const colorChangeHandler = (noteId) => {
+    let indexOfNote = notesState.notesList.findIndex(
+      (note) => note._id == noteId
+    );
+    setTimeout(function () {
+      let notesData = notesState.notesList[indexOfNote];
 
+      updateNoteBgColor({ auth, noteData: notesData, dispatchNotes });
+    }, 500);
+  };
   return (
     <>
       <div className="color-box-container">
+        <i
+          className="material-icons"
+          id="cancel-color-box-btn"
+          onClick={() => setColorBox((colorBox) => !colorBox)}
+        >
+          cancel
+        </i>
         {noteColorsList.map((noteColorName) => (
           <div
             key={uuid()}
             className={`color-select ${noteColorName}`}
-            onClick={() =>
-              dispatchNotes({
-                type: "CHANGE-COLOR",
-                payload: { noteId: _id, colorName: noteColorName },
-              })
-            }
+            onClick={() => {
+              {
+                dispatchNotes({
+                  type: "CHANGE-COLOR",
+                  payload: { noteId: _id, colorName: noteColorName },
+                });
+                colorChangeHandler(_id);
+              }
+            }}
           ></div>
         ))}
       </div>
